5653de18edfa905dd9af5fac5f268efbf38a307d
Isaias Nhantumbo <isaiasnhantumbo54@gmail.com>
2021-09-19 00:44:22 +0200

    :sparkles: Finish animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import FirstAndSecond from './components/FirstAndSecond';
+import Header from './components/Header';
+import { Main, Section, Sticky } from './styles';
+
+import GlobalStyles from './styles/global';
 
 function App() {
+  useEffect(() => {
+    window.scrollTo({ top: 0 });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <head>
+        <title>UI Clone | Typeform</title>
+      </head>
+
+      <GlobalStyles />
+      <Header />
+      <Main style={{ height: '1610vh' }}>
+        <Section style={{ height: '72.7%' }}>
+          <FirstAndSecond />
+        </Section>
+
+        <Section style={{ height: '9.7%' }}>
+          <Sticky className="third" />
+        </Section>
+        <Section style={{ height: '10.1%' }}>
+          <Sticky className="fourth" />
+        </Section>
+      </Main>
+    </>
   );
 }
 
